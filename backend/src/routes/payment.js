1f66f2ca932b1996feefa9c3a91b48822ddd5063
CARLOS CAUÃ ASSUNCAO DE ALMEIDA <232.975989@universidadesenaicimatec.edu.br>
2026-04-09 15:35:07 -0300

    mercado pago atualizado

--- backend/src/routes/payment.js
+++ backend/src/routes/payment.js
@@ -5,6 +5,7 @@ const { MercadoPagoConfig, Preference } = require("mercadopago");
 
 const client = new MercadoPagoConfig({
   accessToken: process.env.MP_ACCESS_TOKEN,
+  environment: "sandbox", // ou "production" para produção
 });
 
 const preference = new Preference(client);
@@ -21,28 +22,23 @@ router.post("/create-checkout", async (req, res) => {
     }
 
     const result = await preference.create({
-      body: {
-        items: [
-          {
-            title: descricao,
-            quantity: 1,
-            unit_price: Number(valor),
-            currency_id: "BRL",
-          },
-        ],
-
-        // URLs de retorno (obrigatório)
-        back_urls: {
-          success: "http://localhost:5173/sucesso",
-          failure: "http://localhost:5173/erro",
-          pending: "http://localhost:5173/pendente",
-        },
-
-        external_reference: "pedido_123",
-        
-        auto_return: "approved", // opcional, pode ser usado para identificar o pedido no seu sistema
+  body: { // <--- Adicione esta chave envolvendo tudo
+    items: [
+      {
+        title: descricao,
+        quantity: 1,
+        unit_price: Number(valor),
+        currency_id: "BRL",
       },
-    });
+    ],
+    back_urls: {
+      success: "http://localhost:5173/sucesso",
+      failure: "http://localhost:5173/erro",
+      pending: "http://localhost:5173/pendente",
+    },
+    
+  }, // <--- Feche a chave body aqui
+});
 
     return res.json({
       init_point: result.init_point,
